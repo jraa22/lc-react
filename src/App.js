201dd39c7c6c37bd122812acedaa2e4053d4270e
Jesús Roberto Ahumada Avendaño <jraa22@gmail.com>
2022-03-02 17:59:38 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { useState } from 'react';
+import Another from './Another';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const increment = () =>{
+    setCount(prevCount => prevCount+1)
+  };
+
+  const decrement = () =>{
+    setCount(prevCount => prevCount-1)
+  };
+
+  const [count, setCount] = useState(0);
+
+  const someStyle = {
+    background: 'red',
+    color: 'white',
+    fontSize: '28px',
+    fontWeight: 'bold'
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <Another name="Robert" />
+        <div>
+          <span>{count}</span>
+          <button onClick={decrement}>-</button>
+          <button onClick={increment}>+</button>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +42,7 @@ function App() {
         >
           Learn React
         </a>
+        <p style={someStyle}>{3 + 2}</p>
       </header>
     </div>
   );
